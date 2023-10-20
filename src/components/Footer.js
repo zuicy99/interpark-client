d2a7b3167f20a45221c08165b0ba1ece2cfccf03
zuicy99 <k.zuicy@gmail.com>
2023-10-20 17:21:56 +0900

    feat : 프로젝트 정리 및 최적화

---
+++ src/components/Footer.js
@@ -0,0 +1,4 @@
+export default function Footer() {
+  //javaScript 코드 자리
+  return <footer class="footer">하단</footer>;
+}
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+export default function Header() {
+  //javaScript 코드 자리
+  return <header class="header">상단</header>;
+}
