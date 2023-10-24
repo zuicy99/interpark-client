c703def79915a43cc14872a5e2233649b70f5388
zuicy99 <k.zuicy@gmail.com>
2023-10-24 16:01:14 +0900

    feat : header영역 기초 완료

--- src/components/Footer.js
+++
@@ -1,4 +0,0 @@
-export default function Footer() {
-  //javaScript 코드 자리
-  return <footer class="footer">하단</footer>;
-}
--- src/components/Header.js
+++
@@ -1,4 +0,0 @@
-export default function Header() {
-  //javaScript 코드 자리
-  return <header class="header">상단</header>;
-}
